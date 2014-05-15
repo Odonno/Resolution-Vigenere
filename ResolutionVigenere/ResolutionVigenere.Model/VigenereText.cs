17dacb11ed5f304ad94f40203a766a2df6f8df58
Odonno <bottiau.david@laposte.net>
2014-05-15 10:05:10 +0200

    Create ViewModel project

---
+++ ResolutionVigenere/ResolutionVigenere.Model/VigenereText.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolutionVigenere.Model
+{
+    public class VigenereText
+    {
+        public string Text { get; set; }
+        public int KeyLength { get; set; }
+        public char[] Key { get; set; }
+    }
+}
